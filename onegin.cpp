4fbe0f647cd0730fcc2daa54978e038c4f03fed7
Lord <lord.nemo@protonmail.com>
2021-09-23 05:03:24 +0300

    some refactoring happend; TODO debug and test

--- onegin.cpp
+++ onegin.cpp
@@ -49,60 +49,14 @@ int main(int argc, char *argv[])
         }
     }
 
-    
-    FILE *inp = fopen(fileName, "r");
-    if (!inp) {
-        printf("Failed to open the file.\n");
-        return 0;
-    }
-
-    fseek(inp, 0, SEEK_END);
-    size_t bufLen = ftell(inp);
-    char *buffer = (char*)calloc(bufLen + 1, sizeof(char));         //TODO free buffer properly
-    fseek(inp, 0, 0);
-    fread(buffer, sizeof(char), bufLen, inp);
-    buffer[bufLen] = 0;
-
-    // printf("%zu\n", wcslen(buffer));
-    // printf(" %s \n==========\n", buffer);
-    // wprintf(L"%ls \n", L"A wide string");
-
-    size_t indexSize = 0;
-    for (size_t i = 0; i < bufLen; ++i) {
-        if (buffer[i] == L'\n') {
-            ++indexSize;
-        }
-    }
-    printf("indexSize = %zu\n", indexSize);
-    if (indexSize == 0) {   //TODO 
-        printf("File is empty?\n");
-        free(buffer);
-        return 0;
-    }
-
-    String* Index = (String*)calloc(indexSize, sizeof(String));        //TODO free index properly
-
-    Index[0].beg = buffer;
-    size_t indexCnt = 0;
-
-    for (size_t i = 0; i < bufLen && indexCnt < indexSize - 1; ++i) {
-        if (buffer[i] == '\n') {
-            printf("indexCnt = %zu\n", indexCnt);
-            Index[indexCnt].len = dist(Index[indexCnt].beg, &buffer[i]);// / sizeof(char); //TODO find why is this constant 
-            ++indexCnt;
-            Index[indexCnt].beg = &buffer[i + 1];
-        }
-    }
-    Index[indexCnt].len = dist(Index[indexCnt].beg, buffer + bufLen - 1);
+    Text text;
+    Text_ctor(&text, fileName);
 
     printf("Here!\n");
-    printIndex(Index, indexSize);
+    Text_dumpIndex(&text);
 
 
-    qsort(Index, indexSize, sizeof(char*), straightComp);
+    qsort(text.Index, text.indexLen, sizeof(char*), straightComp);
     
-    printIndex(Index, indexSize);
-
-    free(Index);
-    free(buffer);
+    Text_dumpIndex(&text);
 }
--- onegin.h
+++ onegin.h
@@ -16,14 +16,134 @@
 #include <math.h>
 #include <ctype.h>
 #include <wchar.h>
+#include <assert.h>
 
 #include "gqsort.h"
 
+static const size_t POINTER_POISON = 13;
+
 struct String {
-    char* beg;
-    size_t len;
+    char*  beg = (char*)POINTER_POISON;
+    size_t len = -1;
 } typedef String;
 
+
+//==================================
+//  Text struct
+
+struct Text {
+    char  *data = (char*)POINTER_POISON;
+    size_t dataLen = -1;
+
+    String *Index = (String*)POINTER_POISON;
+    size_t indexLen = -1;
+} typedef Text;
+
+
+static void Text_ctor(Text *this_, char* fileName) {
+    FILE *inp = fopen(fileName, "r");
+    if (!inp) {
+        printf("Failed to open the file.\n");
+        assert(false);          //TODO think of something better here
+    }
+
+    fseek(inp, 0, SEEK_END);
+    this_->dataLen = ftell(inp);
+    
+
+    #ifndef NDEBUG
+    if (this_->data != (char*)POINTER_POISON) {
+        printf("WARNING: Trying to allocate memory for in-use pointer.\n");
+        assert(false);
+    }
+    #endif
+
+    this_->data = (char*)calloc(this_->dataLen + 1, sizeof(char));         //TODO free buffer properly
+    
+    if (!this_->data) { //TODO check what calloc returns on failure
+        printf("Failed to allocate buffer memory.\n");
+        assert(false);
+    }
+    
+
+    fseek(inp, 0, 0);
+    fread(this_->data, sizeof(char), this_->dataLen, inp);
+    this_->data[this_->dataLen] = 0;
+
+    this_->indexLen = 0;
+    for (size_t i = 0; i < this_->dataLen; ++i) {
+        if (this_->data[i] == L'\n') {
+            ++this_->indexLen;
+        }
+    }
+
+    if (this_->indexLen == 0) {   //TODO 
+        printf("File is empty?\n");
+        free(this_->data);
+        this_->data = (char*)POINTER_POISON;
+        assert(false);
+    }
+
+    
+    #ifndef NDEBUG
+    if (this_->Index != (String*)POINTER_POISON) {
+        printf("WARNING: Trying to allocate memory for in-use pointer.\n");
+        assert(false);
+    }
+    #endif
+
+    this_->Index = (String*)calloc(this_->indexLen, sizeof(String));
+    
+    if (!this_->Index) { //TODO check what calloc returns on failure
+        printf("Failed to allocate buffer memory.\n");
+        assert(false);
+    }
+
+
+    this_->Index[0].beg = this_->data;
+    size_t indexCnt = 0;
+
+    for (size_t i = 0; i < this_->dataLen && indexCnt < this_->indexLen - 1; ++i) {
+        if (this_->data[i] == '\n') {
+            printf("indexCnt = %zu\n", indexCnt);
+            this_->Index[indexCnt].len = dist(this_->Index[indexCnt].beg, &this_->data[i]);// / sizeof(char); //TODO find why is this constant 
+            ++indexCnt;
+            this_->Index[indexCnt].beg = &this_->data[i + 1];
+        }
+    }
+    this_->Index[indexCnt].len = dist(this_->Index[indexCnt].beg, this_->data + this_->dataLen - 1);
+}
+
+static void Text_dumpIndex(Text *this_)         //TODO add cout/cerr pipes support
+{
+    for (size_t i = 0; i < this_->indexLen; ++i) {
+        char* iter = this_->Index[i].beg;
+        while(*iter != '\n') {  
+            printf("%c", *iter);
+            ++iter;
+        }
+        printf("\n size = %zu\n", this_->Index[i].len);
+    }
+}
+
+static void Text_dtor(Text *this_)
+{
+    #ifndef NDEBUG
+    if (this_->data == (char*)POINTER_POISON || this_->Index == (String*)POINTER_POISON) {
+        printf("WARNING: Trying to destroy Text with poisoned pointer.\n");
+        assert(false);
+    }
+    #endif
+
+    free(this_->data);
+    free(this_->Index);
+
+    this_->data = (char*)POINTER_POISON;
+    this_->Index = (String*)POINTER_POISON;
+}
+
+//==================================
+// Compair functions
 
 int straightComp(const void *first, const void *second)         //TODO
 {
